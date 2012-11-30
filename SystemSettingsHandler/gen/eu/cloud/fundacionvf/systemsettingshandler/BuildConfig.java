f45648f25fb4ef3f391d9697460741d9cccfe40e
Alberto <delgado.forni@gmail.com>
2012-11-30 14:14:00 +0100

    Initial Commit for git

---
+++ SystemSettingsHandler/gen/eu/cloud/fundacionvf/systemsettingshandler/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package eu.cloud.fundacionvf.systemsettingshandler;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ SystemSettingsHandler/src/eu/cloud/fundacionvf/systemsettingshandler/SystemService.java
@@ -0,0 +1,30 @@
+package eu.cloud.fundacionvf.systemsettingshandler;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class SystemService extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@Deprecated
+	public void onStart(Intent intent, int startId) {
+		// TODO Auto-generated method stub
+		super.onStart(intent, startId);
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO Auto-generated method stub
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	
+	
+}
