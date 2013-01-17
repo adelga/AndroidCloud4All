92f67830d690e3561dc50cf5a0dc7c4fc32a4c79
Alberto <delgado.forni@gmail.com>
2013-01-17 12:10:33 +0100

    Fisrt Upload

--- SystemSettingsHandler/src/eu/cloud/fundacionvf/systemsettingshandler/SystemService.java
+++ SystemSettingsHandler/src/eu/cloud/fundacionvf/systemsettingshandler/SystemService.java
@@ -2,10 +2,12 @@ package eu.cloud.fundacionvf.systemsettingshandler;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 
 public class SystemService extends Service {
-
+	private final String TAG = "SystemSettingHandler";
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
@@ -17,11 +19,17 @@ public class SystemService extends Service {
 	public void onStart(Intent intent, int startId) {
 		// TODO Auto-generated method stub
 		super.onStart(intent, startId);
+		Log.d(TAG, "ssssssssssssssssssssssssssssssssssss");
 	}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		// TODO Auto-generated method stub
-		return super.onStartCommand(intent, flags, startId);
+		Bundle extras = intent.getExtras();
+		Log.d(TAG, extras.getString("Params"));
+		
+		return START_STICKY;
+		
+		
 	}
 }
