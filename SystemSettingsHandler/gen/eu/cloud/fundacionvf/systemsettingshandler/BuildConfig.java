13d4864e7d6e1ddcef01457fd5eb4eb8e9f2dc62
Alberto <delgado.forni@gmail.com>
2013-01-18 13:42:29 +0100

    Eliminamos el System que no funciona

--- SystemSettingsHandler/gen/eu/cloud/fundacionvf/systemsettingshandler/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package eu.cloud.fundacionvf.systemsettingshandler;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
