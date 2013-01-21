89d8952f3c6970eb89fc8058209ca190e9a63341
Alberto <delgado.forni@gmail.com>
2013-01-21 14:46:00 +0100

    Corregido problema con la extension de la lista de ids

--- Receiver/src/test/receiver/MyService.java
+++ Receiver/src/test/receiver/MyService.java
@@ -1,24 +1,25 @@
 package test.receiver;
 
+import org.json.JSONException;
+
+import test.receiver.persistence.CommunicationPersistence;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
-public class MyService extends Service{
+public class MyService extends Service {
 
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
-		
-		
+
 		return null;
 	}
 
 	@Override
 	public void onCreate() {
 		// TODO Auto-generated method stub
-		Log.e("Receiver", "vamosssss");
 		super.onCreate();
 	}
 
@@ -31,11 +32,39 @@ public class MyService extends Service{
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		// TODO Auto-generated method stub
-		Log.e("Receiver", "startComand");	
-		CloudIntent cloudinfo= CloudIntent.intentToCloudIntent(intent);
-		
-		Log.e("Receiver", "idevento: " + cloudinfo.getIdEvent());
+		try {
+			Log.e("Receiver", "startComand");
+			CloudIntent cloudinfo = CloudIntent.intentToCloudIntent(intent);
+			int event = cloudinfo.getIdEvent();
+			switch (event) {
+			case CommunicationPersistence.EVENT_CONFIGURE_SYSTEM_SETTINGS:
+				configurar(cloudinfo);
+				break;
+
+			default:
+				break;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return super.onStartCommand(intent, flags, startId);
+	}
+
+	private void configurar(CloudIntent cloudinfo) {
+		// TODO Auto-generated method stub
+		String[] listaids;
+		try {
+			listaids = cloudinfo.getArrayIds();
+
+			for (int i = 0; i < listaids.length; i++){
+				Log.e("Receiver", "Parametros: id: "+ listaids[i] + ", value: "+ cloudinfo.getValue(listaids[i]));
+				
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
