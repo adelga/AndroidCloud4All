9ad044f929b353f7d28a13ce614957f4adb8ce0a
Alberto <delgado.forni@gmail.com>
2013-03-06 13:46:45 +0100

    Comments

--- Receiver/src/test/receiver/MyService.java
+++ Receiver/src/test/receiver/MyService.java
@@ -1,3 +1,22 @@
+/*
+MyServivce class
+This class extends from Service, the intent is reading and handler different acctions for each EVENT.	
+
+Copyright (c) 2013, Vodafone Spain Foundation
+All rights reserved.
+
+The research leading to these results has received funding from the European Union's Seventh Framework Programme (FP7/2007-2013) under grant agreement n° 289016
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of Vodafone Spain Foundation nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
 package test.receiver;
 
 import org.json.JSONException;
@@ -34,8 +53,11 @@ public class MyService extends Service {
 		// TODO Auto-generated method stub
 		try {
 			Log.e("Receiver", "startComand");
+			//Transform Intent in a CloudIntent instance.
 			CloudIntent cloudinfo = CloudIntent.intentToCloudIntent(intent);
+			//Obtain the event and planing
 			int event = cloudinfo.getIdEvent();
+			
 			switch (event) {
 			case CommunicationPersistence.EVENT_CONFIGURE_SYSTEM_SETTINGS:
 				configurar(cloudinfo);
@@ -50,7 +72,9 @@ public class MyService extends Service {
 		}
 		return super.onStartCommand(intent, flags, startId);
 	}
-
+/*
+ * Method that shows the params lists for this communication example
+ */
 	private void configurar(CloudIntent cloudinfo) {
 		// TODO Auto-generated method stub
 		String[] listaids;
