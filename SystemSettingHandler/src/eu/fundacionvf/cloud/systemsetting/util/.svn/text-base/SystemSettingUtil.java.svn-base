cc259efd20cb53c3be7a4df2462484cbe67e0ef8
Alberto <delgado.forni@gmail.com>
2013-03-21 12:56:08 +0100

    Comments and readme

--- SystemSettingHandler/src/eu/fundacionvf/cloud/systemsetting/util/.svn/text-base/SystemSettingUtil.java.svn-base
+++ SystemSettingHandler/src/eu/fundacionvf/cloud/systemsetting/util/.svn/text-base/SystemSettingUtil.java.svn-base
@@ -1,3 +1,23 @@
+/*
+SystemSettingHandler
+This class implements the funcionality to modify the system Settings as the screen brightness or time off screen.
+
+Copyright (c) 2013, Vodafone Spain Foundation
+All rights reserved.
+
+The research leading to these results has received funding from the European Union's Seventh Framework Programme (FP7/2007-2013) under grant agreement n° 289016
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of Vodafone Spain Foundation nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+
 package eu.fundacionvf.cloud.systemsetting.util;
 
 import eu.fundacionvf.cloud.systemsetting.DummyActivity;
@@ -23,6 +43,14 @@ public class SystemSettingUtil {
 
 		this.cntx = cntx;
 	}
+	
+	/**
+	 * Configure brightness screen settings
+	 * This method needs a instance of SystemSettingUtil because the context is necessary
+	 * @param mode, 0 if the brightness mode is automatic, 1 if it's manual.
+	 * @param level, if the mode is MANUAL, the level of brightness since 0(min) to 255 (max)
+	 * @return void
+	 */
 
 	public void changeBrightness(int mode, int level) {
 		try {
@@ -103,6 +131,12 @@ public class SystemSettingUtil {
 
 	}
 
+	/**
+	 * Configure the screen time off
+	 * This method needs a instance of SystemSettingUtil because the context is necessary
+	 * @param timeoff, time of inactivity before turn off the screen.
+	 * @return void
+	 */
 	public void changeTimeScreenOff(String timeoff) {
 		try {
 			SharedPreferences prefs = cntx.getSharedPreferences(preferences,
@@ -138,6 +172,13 @@ public class SystemSettingUtil {
 
 	}
 
+	/**
+	 * Configure the screen time dim
+	 * This method needs a instance of SystemSettingUtil because the context is necessary
+	 * @param dim, boolean, if it's true the screen put dim mode before turn off.
+	 * @return void
+	 */
+	
 	public void changeDimScreen(boolean dim) {
 		try {
 			SharedPreferences prefs = cntx.getSharedPreferences(preferences,
--- SystemSettingHandler/src/eu/fundacionvf/cloud/systemsetting/util/SystemSoundsUtil.java
+++ SystemSettingHandler/src/eu/fundacionvf/cloud/systemsetting/util/SystemSoundsUtil.java
@@ -1,3 +1,23 @@
+/*
+SystemSoundUtil class
+This class implements the funcionality to configure all general sounds, as default tones and System Sounds as lock Screen sound. 
+
+Copyright (c) 2013, Vodafone Spain Foundation
+All rights reserved.
+
+The research leading to these results has received funding from the European Union's Seventh Framework Programme (FP7/2007-2013) under grant agreement n° 289016
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of Vodafone Spain Foundation nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+
 package eu.fundacionvf.cloud.systemsetting.util;
 
 import java.io.BufferedInputStream;
@@ -31,14 +51,24 @@ public class SystemSoundsUtil {
 
 		this.cntx = cntx;
 	}
-
+	/**
+	 * Configure the default notification Sound.
+	 * This method needs a instance of SystemSoundUtil because the context is necessary.
+	 * Coming soon, there will be special sounds in our server for different user hear needs.
+	 * @param urldound, change the default Notification Sound for a file download on own server 
+	 * @return void
+	 * 
+	 */
 	public void changeNotificationSound(String urlsound) {
-		String fileName = Environment.getExternalStorageState()+ "/cloud4AllSound/" + "notificationsound.ogg";
-
+		String path = Environment.getExternalStorageDirectory()+ "/cloud4AllSound/";
+        String fileName = "notificationsound.ogg";
 		try{
             URL url = new URL(urlsound); //you can write here any link
-            File file = new File(fileName);
+            File myDir = new File(path);
+            myDir.mkdir();
+            File file = new File(path,fileName);
 			Log.d("PREFERENCES", "Run hilo");
+			Log.i("PREFERENCES", "filename:"+fileName);
 
 				
 	            /* Open a connection to that URL. */
@@ -75,7 +105,7 @@ public class SystemSoundsUtil {
 		}
 		
 		try{
-			String path = Environment.getExternalStorageState()+ "/cloud4AllSound/";
+		//	String path = Environment.getExternalStorageState()+ "/cloud4AllSound/";
 			File k = new File(path, "notificationsound.ogg"); // path is a file to /sdcard/media/ringtone
 
 		SharedPreferences prefs = cntx.getSharedPreferences(preferences,
@@ -94,13 +124,15 @@ public class SystemSoundsUtil {
 		values.put(MediaStore.Audio.Media.IS_MUSIC, false);
 
 		String olduri = Settings.System.getString(
-				this.cntx.getContentResolver(), Settings.System.RINGTONE);
+				this.cntx.getContentResolver(), Settings.System.NOTIFICATION_SOUND);
 		Log.d(tag, "old uri: " + olduri.toString());
 
 		// Storage in SharedPreferences
-
-		prefs.edit().putString("NOTIFICATION_SOUND", olduri);
-		prefs.edit().commit();
+		SharedPreferences.Editor editor = prefs.edit();
+		//prefs.edit().putString("NOTIFICATION_SOUND", olduri);
+		//prefs.edit().commit();
+		editor.putString("NOTIFICATION_SOUND", olduri);
+		editor.commit();
 
 		// Insert it into the database
 		Uri uri = MediaStore.Audio.Media.getContentUriForPath(k
@@ -124,6 +156,7 @@ public class SystemSoundsUtil {
 			SharedPreferences prefs = cntx.getSharedPreferences(preferences,
 					Context.MODE_PRIVATE);
 			String olduri = prefs.getString("NOTIFICATION_SOUND", "");
+			Log.i("PREFERENCES", "olduri:"+olduri);
 			Uri uri = Uri.parse(olduri);
 			if (!olduri.equals("")) {
 				// put tone in the default (It's the same that write in System
@@ -142,7 +175,14 @@ public class SystemSoundsUtil {
 
 	}
 	
-	
+	/**
+	 * Configure the default ringtone Sound.
+	 * This method needs a instance of SystemSoundUtil because the context is necessary.
+	 * Coming soon, there will be special sounds in our server for different user hear needs.
+	 * @param urldound, change the default Ringtone Sound for a file download on own server 
+	 * @return void
+	 * 
+	 */
 	public void changeRingtoneSound(String urlsound) {
 		String fileName = Environment.getExternalStorageState()+ "/cloud4AllSound/" + "ringtonesound.ogg";
 
