5dfeb1874287e33416d23e64f3d659454e157de1
FVE <FVE@FVE-Desarrollo-Manuel>
2013-04-19 10:47:48 +0200

    License Readme text added

--- Orchestrator/src/eu/fundacionvf/cloud4all/orchestrator/persistence/Persistence.java
+++ Orchestrator/src/eu/fundacionvf/cloud4all/orchestrator/persistence/Persistence.java
@@ -1,3 +1,21 @@
+/*
+Persistence class
+This class is part of the Persistence Object
+
+Copyright (c) 2013, Vodafone Spain Foundation
+All rights reserved.
+
+The research leading to these results has received funding from the European Union's Seventh Framework Programme (FP7/2007-2013) under grant agreement n° 289016
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of Vodafone Spain Foundation nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
 package eu.fundacionvf.cloud4all.orchestrator.persistence;
 
 import org.simpleframework.xml.ElementArray;
@@ -5,49 +23,34 @@ import org.simpleframework.xml.Root;
 
 @Root
 public class Persistence {
-	//@ElementList
-	//ArrayList<Trigger> triggers = new ArrayList<Trigger>();
-	//@ElementList
-	//ArrayList<Process> processes = new ArrayList<Process>();
 	@ElementArray
 	private Trigger[] triggers;
 	@ElementArray
 	private Process[] processes;
-	
-	public Persistence(){}
-	
-	/*public ArrayList<Trigger> getTriggers() {
-		return triggers;
-	}*/
-	public Trigger[] getTriggers(){
+
+	public Persistence() {
+	}
+
+	public Trigger[] getTriggers() {
 		return triggers;
 	}
 
-	/*public void setTriggers(int orderNumberTrigger,List<Trigger> triggers) {
-		this.triggers = triggers;
-	}*/
-
-	/*public ArrayList<Process> getProcesses() {
-		return processes;
-	}*/
-	public Process[] getProcesses(){
+	public Process[] getProcesses() {
 		return processes;
 	}
-	public void setProcess(Process process, int index){
-	    this.processes[index] = process; // put the instance in the correct bucket
+
+	public void setProcess(Process process, int index) {
+		this.processes[index] = process; // put the instance in the correct
+											// bucket
 	}
 
-/*	public void setProcesses(int orderNumberProcess,List<Process> processes) {
-		this.processes = processes;
-	}*/
-	
-	//UTIL METHODS
-	public int findIdProccessByIdTrigger(int idTrigger){
+	// UTIL METHODS
+	public int findIdProccessByIdTrigger(int idTrigger) {
 		int result = -1;
 		int i = 0;
 		boolean founded = false;
-		while(i<triggers.length && !founded){
-			if(triggers[i].getIdTrigger() == idTrigger){
+		while (i < triggers.length && !founded) {
+			if (triggers[i].getIdTrigger() == idTrigger) {
 				result = triggers[i].getIdProcess();
 				founded = true;
 			}
@@ -55,12 +58,13 @@ public class Persistence {
 		}
 		return result;
 	}
-	public Process getProcessByIdProcess(int idProcess){
+
+	public Process getProcessByIdProcess(int idProcess) {
 		int i = 0;
 		Process process = null;
 		boolean founded = false;
-		while(i<processes.length && !founded){
-			if(processes[i].getIdProcess() == idProcess){
+		while (i < processes.length && !founded) {
+			if (processes[i].getIdProcess() == idProcess) {
 				process = processes[i];
 				founded = true;
 			}
@@ -68,13 +72,14 @@ public class Persistence {
 		}
 		return process;
 	}
-	public Action[] getActionsByIdTrigger(int idTrigger){
+
+	public Action[] getActionsByIdTrigger(int idTrigger) {
 		int idProccess = findIdProccessByIdTrigger(idTrigger);
 		int i = 0;
 		Action[] actions = null;
 		boolean founded = false;
-		while(i<processes.length && !founded){
-			if(processes[i].getIdProcess() == idProccess){
+		while (i < processes.length && !founded) {
+			if (processes[i].getIdProcess() == idProccess) {
 				actions = processes[i].getListActions();
 				founded = true;
 			}
@@ -82,11 +87,12 @@ public class Persistence {
 		}
 		return actions;
 	}
-	public boolean isRequest(int idTrigger){
+
+	public boolean isRequest(int idTrigger) {
 		boolean request = false;
-		int i=0;
-		while(i<triggers.length && !request){
-			if(triggers[i].getIdTrigger() == idTrigger){
+		int i = 0;
+		while (i < triggers.length && !request) {
+			if (triggers[i].getIdTrigger() == idTrigger) {
 				request = true;
 			}
 			i++;
--- Orchestrator/src/eu/fundacionvf/cloud4all/orchestrator/persistence/PersistenceXML.java
+++ Orchestrator/src/eu/fundacionvf/cloud4all/orchestrator/persistence/PersistenceXML.java
@@ -1,3 +1,21 @@
+/*
+PersistenceXML class
+This class manage the read and write of persistence and session file
+
+Copyright (c) 2013, Vodafone Spain Foundation
+All rights reserved.
+
+The research leading to these results has received funding from the European Union's Seventh Framework Programme (FP7/2007-2013) under grant agreement n° 289016
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of Vodafone Spain Foundation nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
 package eu.fundacionvf.cloud4all.orchestrator.persistence;
 
 import java.io.File;
@@ -7,19 +25,18 @@ import org.simpleframework.xml.core.Persister;
 import android.os.Environment;
 
 public class PersistenceXML extends Persistence {
-	private final static String FILENAME = Environment.getExternalStorageDirectory().getPath()+"/download/Persistence.xml";
-	private final static String FILENAME_SESSION = Environment.getExternalStorageDirectory().getPath()+"/download/Session.xml";
+	private final static String FILENAME = Environment.getExternalStorageDirectory().getPath()+ "/download/Persistence.xml";
+	private final static String FILENAME_SESSION = Environment.getExternalStorageDirectory().getPath() + "/download/Session.xml";
 	Persistence p = new Persistence();
 	Session s = new Session();
-	//Persistence s = new Persistence();
 	Serializer serializer = new Persister();
 
 	public PersistenceXML() {
 
 		// Start XML Parser
-		//Load Persistence XML
+		// Load Persistence XML
 		File source = new File(FILENAME);
-		if(!source.exists()){
+		if (!source.exists()) {
 			try {
 				source.createNewFile();
 			} catch (IOException e) {
@@ -33,9 +50,9 @@ public class PersistenceXML extends Persistence {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//Load Session XML
+		// Load Session XML
 		File sessionSource = new File(FILENAME_SESSION);
-		if(!sessionSource.exists()){
+		if (!sessionSource.exists()) {
 			try {
 				sessionSource.createNewFile();
 			} catch (IOException e) {
@@ -43,10 +60,9 @@ public class PersistenceXML extends Persistence {
 				e.printStackTrace();
 			}
 		}
-		try{
+		try {
 			s = serializer.read(Session.class, sessionSource);
-		//	s = serializer.read(Persistence.class, sessionSource);
-		} catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -54,11 +70,12 @@ public class PersistenceXML extends Persistence {
 	public Persistence getPersistence() {
 		return p;
 	}
-	
-	public Session getSession(){
+
+	public Session getSession() {
 		return s;
 	}
-	public void writeXML(Session s){
+
+	public void writeXML(Session s) {
 		File result = new File(FILENAME_SESSION);
 		try {
 			serializer.write(s, result);
