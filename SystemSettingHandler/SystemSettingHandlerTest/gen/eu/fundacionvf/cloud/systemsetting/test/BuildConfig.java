0ddd4fbd3cba32c685e398e5b17fa35cd9b5e634
Alberto <delgado.forni@gmail.com>
2013-07-26 09:28:06 +0200

    Test no root added

---
+++ SystemSettingHandler/SystemSettingHandlerTest/gen/eu/fundacionvf/cloud/systemsetting/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package eu.fundacionvf.cloud.systemsetting.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
